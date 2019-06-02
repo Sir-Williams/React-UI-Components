88fc08e65fdecbca54011466f7e227d993bf86df
Sir-Williams <sirjohnwilliams@live.com>
2019-06-02 11:45:20 -0700

    added finishing code to all js files; all components show on screen; need to make css

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,34 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from "../ButtonComponents/NumberButton";
+import ActionButton from "../ButtonComponents/ActionButton";
+import {numPadNumbers} from "../numbers/numbers";
+import {numPadAction} from "../numbers/numbers";
+
+let answer = 0;
+
+function CalculatorDisplay() {
+    return (
+        <div className="calcContainer">
+            <div className="calcAnswer">0</div>
+            <div className="numpad">
+                <div className="clearBtn">clear</div>
+                    {
+                        numPadNumbers.map( num => {
+                            return <NumberButton class="numBtn" text={num.text} />
+                        })
+                    }
+                <div className="answer">0</div>
+            </div>
+            <div className="actionPad">
+                {
+                    numPadAction.map(action => {
+                        return <ActionButton className="actionBtn" text={action.text} />
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default CalculatorDisplay;
---
+++ Project-II-Calculator/calculator/src/components/numbers/numbers.js
@@ -0,0 +1,65 @@
+export const numPadNumbers = [
+    {
+        "number": "zero",
+        "text": 0
+    },
+    {
+        "number": "one",
+        "text": 1
+    },
+    {
+        "number": "two",
+        "text": 2
+    },
+    {
+        "number": "three",
+        "text": 3
+    },
+    {
+        "number": "four",
+        "text": 4
+    },
+    {
+        "number": "five",
+        "text": 5
+    },
+    {
+        "number": "six",
+        "text": 6
+    },
+    {
+        "number": "seven",
+        "text": 7
+    },
+    {
+        "number": "eight",
+        "text": 8
+    },
+    {
+        "number": "nine",
+        "text": 9
+    }
+]
+
+export const numPadAction = [
+    {
+        "action": 'add',
+        "text": "+"
+    },
+    {
+        "action": 'subtract',
+        "text": "-"
+    },
+    {
+        "action": 'multiply',
+        "text": "x"
+    },
+    {
+        "action": 'divide',
+        "text": "÷"
+    },
+    {
+        "action": 'equal',
+        "text": "="
+    }
+]
